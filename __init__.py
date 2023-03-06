3558f679608b3b64cdde06db6b74a0ff98e2efd8
takecccc <takecccc3@gmail.com>
2023-03-07 01:31:28 +0900

    versionを1.1に変更

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "VertexGroup Renamer",
     "author": "takec",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (3, 4, 0),
     "location": "View3D > Tool Shelf > Renamer > VertexGroup Renamer",
     "description": "Renames vertex groups based on bone mappings",
